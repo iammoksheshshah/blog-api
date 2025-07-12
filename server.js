ea3a6b92f0b1cce1bc652586534980c321ced0d1
iammoksheshshah <moksheshshah2311@gmail.com>
2025-07-12 14:32:29 +0530

    setup

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const landingRoutes = require('./routes/landing.routes');
 const blogRoutes = require('./routes/blog.routes');
 const { errorHandler } = require('./middlewares/error.middleware');
 
+const PORT = process.env.PORT || 5000;
+
 dotenv.config();
 const app = express();
 
@@ -22,6 +24,6 @@ app.use(errorHandler);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`));
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   })
   .catch(err => console.log(err));
